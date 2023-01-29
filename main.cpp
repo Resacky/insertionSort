a6a1d926eaf8b7d3dba3c5a5ab4b29d62181b250
Resacky <Ruiz-joseph@hotmail.com>
2023-01-29 18:54:34 -0500

    update
    update

--- main.cpp
+++ main.cpp
@@ -16,16 +16,23 @@ int main() {
 }
 
 void insertionSort(int *a, int arraySize) {
-    int j, key;
-    for (int i = 0; i < arraySize - 1; i++) {
-        key = a[i];
-        j = i - 1;
-        while (j >= 0 && a[j] > key)
-        {
+    int current;
+    for (int i = 0; i < arraySize; i++) {
+        current = a[i];
+        // previous index of the array, to signify the sorted side of the array and the unsorted side.
+        int j = i - 1;
+
+        // the j >= 0 condition is to check if i is within the first element of the array,
+        // and the a[j] > current condition is to check if the element previous to i is greater than a[i]
+        // if it is then it will shift all elements before i to the right
+        while (j >= 0 && a[j] > current) {
+            // This is to shift all the elements greater than a[j] to the right within the sorted side of the array.
             a[j + 1] = a[j];
-            j = j - 1;
+            j--;
+            // this should repeat until an element within the sorted array is less than the current element of i to then
+            // insert it within the array, since a[i], got hypothetically overwritten with a[j]
         }
-        a[j + 1] = key;
+        a[j + 1] = current;
     }
 }
 
